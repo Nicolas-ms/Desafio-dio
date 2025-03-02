8e2024f795569a8f28fbcd9a8eccad1ffab1bc57
Nicolas <nicolasms721@gmail.com>
2025-03-02 02:08:38 -0300

    Ajeitando texto

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class App {
+public class ContaTerminal {
     public static void main(String[] args) throws Exception {
     //Scanner para ler as entradas do usuário
     Scanner sc = new Scanner(System.in);
@@ -14,7 +14,7 @@ public class App {
     double saldo = Double.parseDouble(sc.nextLine()); 
     
     //Mostrar as informações para o usuário
-    System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é" + agencia + ", conta" + numero + "e seu saldo"+ saldo +"já está disponível para saque");
+    System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo "+ saldo +" já está disponível para saque");
     sc.close();
     }
 }
